6b8eb04375332c1f526fc83d883e04cc5d35dc43
“Joseph <josephcuz@gmail.com>
2023-05-04 18:27:03 -0400

    added guest stuff 5/4

--- src/main/java/edu/yu/cs/Guest.java
+++ src/main/java/edu/yu/cs/Guest.java
@@ -1,4 +1,18 @@
 package edu.yu.cs;
 
 public class Guest {
+    Room myRoom;
+    String name;
+    int age;
+    int ID;
+    int phoneNumber;
+    int moneySpent;
+    int daysBooked;
+    int daysLeftInRoom;
+    int checkInTime;
+    int creditCard;
+
+
+
+
 }
